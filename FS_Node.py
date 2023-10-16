ca6e6de671c32f4069fdcc021061e26de99fb701
Fábio Cunha <a105089@alunos.uminho.pt>
2023-10-16 17:51:21 +0100

    alterações node

--- FS_Node.py
+++ FS_Node.py
@@ -2,49 +2,38 @@ import os
 import sys
 import socket
 import threading
+import hashlib
 
-# Check if the correct number of command-line arguments is provided
-if len(sys.argv) != 2:
-    print("Usage: FS_Node <share_folder>")
+if len(sys.argv) != 3:
+    print("Usage: FS_Node <share_folder> <address> <port>")
     sys.exit(1)
 
-# Extract command-line arguments
-share_folder = sys.argv[1]
-
-# Define the address and port for the FS_Tracker server
-tracker_host = "tracker_server_ip"
+shared_folder = sys.argv[1]
+tracker_host = sys.argv[2]
 tracker_port = 9090
-
-# Define the UDP port for file transfer
+if len(sys.argv) == 4:
+    tracker_port = sys.argv[3]
 udp_port = 9090
-
+block_size = 128
 
 class FS_Node:
-    def __init__(self, shared_dir):
-        self.shared_dir = shared_dir
+    def __init__(self):
+        self.available_blocks = {}  # Dictionary to track block availability
+        self.is_file_divided()
         self.node_info = {
             "address": "",
-            "files": self.get_files_in_shared_dir(),
+            "blocks": self.available_blocks,
         }
 
-    def get_files_in_shared_dir(self):
-        files = []
-        if os.path.exists(self.shared_dir) and os.path.isdir(self.shared_dir):
-            files = [f for f in os.listdir(self.shared_dir) if os.path.isfile(os.path.join(self.shared_dir, f))]
-        return files
-
-    # Create a TCP socket to connect to the FS_Tracker server
     def connect_to_tracker(self):
         fs_node_tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
         try:
-            # Connect to the tracker
             fs_node_tcp.connect((tracker_host, tracker_port))
 
             # Update node_info with the correct address
             self.node_info["address"] = fs_node_tcp.getsockname()
 
-            # Announce presence and share file information with the tracker
             fs_node_tcp.send(str(self.node_info).encode())
 
             while True:
@@ -54,7 +43,31 @@ class FS_Node:
         except Exception as e:
             print(f"Error connecting to the tracker: {e}")
 
-    # Create a UDP socket for file transfer
+    def is_file_divided(self):
+        for root, _, files in os.walk(shared_folder):
+            for file in files:
+                file_path = os.path.join(root, file)
+                if not os.path.isfile(file_path):
+                    continue
+                self.divide_file_into_blocks(file_path)
+
+    def divide_file_into_blocks(self, file_path):
+        with open(file_path, 'rb') as file:
+            block_number = 0
+            while True:
+                data = file.read(block_size)
+                if not data:
+                    break
+                block_id = hashlib.md5(data).hexdigest()  # Unique identifier for the block
+                self.available_blocks[block_id] = {
+                    "owner": self.node_info["address"],
+                    "available": True,
+                }
+                block_path = os.path.join(shared_folder, block_id)
+                with open(block_path, 'wb') as block_file:
+                    block_file.write(data)
+                block_number += 1
+
     def file_transfer(self):
         fs_node_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         fs_node_udp.bind(('', udp_port))
@@ -65,10 +78,8 @@ class FS_Node:
             # Process the request and transfer the file
             pass
 
-
-# Run the FS_Node application using threads
 if __name__ == "__main__":
-    fs_node = FS_Node(share_folder)
+    fs_node = FS_Node()
 
     tracker_thread = threading.Thread(target=fs_node.connect_to_tracker)
     tracker_thread.start()
